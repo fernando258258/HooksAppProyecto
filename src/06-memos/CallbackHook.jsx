66cb5ee1f338376482b93651e874e833f37a9beb
Fernando Ricaldes <fernandorashed258@gmail.com>
2025-07-23 19:01:30 -0400

    useCallback con argumentos

--- src/06-memos/CallbackHook.jsx
+++ src/06-memos/CallbackHook.jsx
@@ -1,12 +1,16 @@
-import { useCallback, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { ShowIncrement } from "./ShowIncrement";
 
 export const CallbackHook = () => {
   const [counter, setCounter] = useState(10);
 
-  const incrementFather = useCallback(() => {
-    setCounter((value) => value + 1);
+  const incrementFather = useCallback((value) => {
+    setCounter((c) => c + value);
   }, []);
+
+  useEffect(() => {
+    // incrementFather();
+  }, [incrementFather]);
 
   //   const incrementFather = () => {
   //     setCounter(counter + 1);
