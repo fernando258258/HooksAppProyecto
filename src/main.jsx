50a32d482a3f07b6669c93b478788da300a98585
Fernando Ricaldes <fernandorashed258@gmail.com>
2025-07-23 18:38:39 -0400

    useMemo

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import { MemoHook } from "./06-memos/MemoHook";
 // import { FocusScreen } from "./04-useRef/focusScreen";
 // import { Layout } from "./05-useLayoutEffect/Layout";
-import { Memorize } from "./06-memos/Memorize";
+// import { Memorize } from "./06-memos/Memorize";
 // import { MultipleCustomHook } from "./03-examples/MultipleCustomHook";
 // import { FormWithCustomHook } from "./02-useEffect/FormWithCustomhook";
 // import { SimpleForm } from "./02-useEffect/SimpleForm";
@@ -18,6 +19,7 @@ createRoot(document.getElementById("root")).render(
     {/* <MultipleCustomHook /> */}
     {/* <FocusScreen /> */}
     {/* <Layout /> */}
-    <Memorize />
+    {/* <Memorize /> */}
+    <MemoHook />
   </StrictMode>
 );
