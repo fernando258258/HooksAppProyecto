2c4ddd07815f78c846dabf0282a307beb46e25fe
Fernando Ricaldes <fernandorashed258@gmail.com>
2025-07-23 18:55:58 -0400

    useCallback

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import { MemoHook } from "./06-memos/MemoHook";
+// import { MemoHook } from "./06-memos/MemoHook";
+import { CallbackHook } from "./06-memos/Callbackhook";
 // import { FocusScreen } from "./04-useRef/focusScreen";
 // import { Layout } from "./05-useLayoutEffect/Layout";
 // import { Memorize } from "./06-memos/Memorize";
@@ -20,6 +21,7 @@ createRoot(document.getElementById("root")).render(
     {/* <FocusScreen /> */}
     {/* <Layout /> */}
     {/* <Memorize /> */}
-    <MemoHook />
+    {/* <MemoHook /> */}
+    <CallbackHook />
   </StrictMode>
 );
