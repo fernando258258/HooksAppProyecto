548330188a661510d6745e9542d071f26f591e5f
Fernando Ricaldes <fernandorashed258@gmail.com>
2025-07-28 10:23:32 -0400

    Idea general de un reducer - Via codigo

--- src/main.jsx
+++ src/main.jsx
@@ -1,30 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-// import { MemoHook } from "./06-memos/MemoHook";
-// import { CallbackHook } from "./06-memos/Callbackhook";
-// import { Padre } from "./07-tarea-memo/Padre";
-import "./08-useReduce/intro-reducer";
-// import { FocusScreen } from "./04-useRef/focusScreen";
-// import { Layout } from "./05-useLayoutEffect/Layout";
-// import { Memorize } from "./06-memos/Memorize";
-// import { MultipleCustomHook } from "./03-examples/MultipleCustomHook";
-// import { FormWithCustomHook } from "./02-useEffect/FormWithCustomhook";
-// import { SimpleForm } from "./02-useEffect/SimpleForm";
-// import { HooksApp } from "./HooksApp";
-// import { CounterApp } from "./01-useState/CounterApp";
-// import { CounterWithCustomHook } from "./01-useState/CounterWithCustomHook";
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    {/* <HooksApp /> */}
-    {/* <FormWithCustomHook /> */}
-    {/* <MultipleCustomHook /> */}
-    {/* <FocusScreen /> */}
-    {/* <Layout /> */}
-    {/* <Memorize /> */}
-    {/* <MemoHook /> */}
-    {/* <CallbackHook /> */}
-    {/* <Padre /> */}
-  </StrictMode>
-);
+import "./08-useReducer/intro-reducer";
+
+createRoot(document.getElementById("root")).render(<StrictMode></StrictMode>);
